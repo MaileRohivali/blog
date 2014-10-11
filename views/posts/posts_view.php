08903719e23ceb846f2bd9ad9a570280866632ba
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-10-11 18:38:57 +0300

    Fixed comments

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -18,10 +18,10 @@
         </div>
     </article>
 </div>
-<? foreach ($comments as $comment): ?>
 <div class="commentBox">
     <ul class="commentList">
-        <li>
+<? foreach ($comments as $comment): ?>
+    <li>
             <div class="commenterImage">
                 <img src="http://lorempixel.com/50/50/people/6" />
             </div>
@@ -31,15 +31,15 @@
                 <p><?=$comment['comment_text']?></p>
             </div>
         </li>
+<? endforeach ?>
     </ul>
     <form class="form-inline" method="post" role="form">
-                <div class="form-group">
-            <input class="form-control" type="text" placeholder="Your comments" name="data[comment_text]"/>
-        </div>
+        <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Your comments" name="data[comment_text]"/>
+                </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">Add</button>
         </div>
     </form>
 </div>
 
-<? endforeach ?>
